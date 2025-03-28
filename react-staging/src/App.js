e2faf9a0d0db14b1bc284473df35fc59e343c587
youngs <lixiang@popicorns.com>
2025-03-28 16:29:21 +0800

    TodoList案例_动态初始化列表

--- react-staging/src/App.js
+++ react-staging/src/App.js
@@ -1,19 +1,30 @@
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
 import List from "./components/List";
 import Footer from "./components/Footer";
 
-function App() {
-  return (
-    <div className="todo-container">
-      <div className="todo-wrap">
-        <Header />
-        <List />
-        <Footer />
+class App extends Component {
+  state = {
+    todos: [
+      { id: 1, name: "吃饭", done: true },
+      { id: 2, name: "睡觉", done: true },
+      { id: 3, name: "打豆豆", done: false },
+    ],
+  };
+  render() {
+    const { todos } = this.state;
+    return (
+      <div className="todo-container">
+        <div className="todo-wrap">
+          <Header />
+          <List todos={todos} />
+          <Footer />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
