9da8c3977f737f799b1d06af082a2fe4516e4c20
youngs <lixiang@popicorns.com>
2025-03-29 10:02:21 +0800

    feat:todolist 代码

---
+++ react-staging/03_src_todoList案例/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import List from "./components/List";
+import Footer from "./components/Footer";
+
+class App extends Component {
+  state = {
+    todos: [
+      { id: 1, name: "吃饭", done: true },
+      { id: 2, name: "睡觉", done: true },
+      { id: 3, name: "打豆豆", done: false },
+    ],
+  };
+  addTodo = (todoObj) => {
+    const { todos } = this.state;
+    this.setState({ todos: [todoObj, ...todos] });
+  };
+  updateTodo = (id, done) => {
+    const { todos } = this.state;
+    this.setState({
+      todos: todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, done };
+        }
+        return todo;
+      }),
+    });
+  };
+  deleteTodo = (id) => {
+    const { todos } = this.state;
+    this.setState({ todos: todos.filter((todo) => todo.id !== id) });
+  };
+  checkAllTodo = (done) => {
+    const { todos } = this.state;
+    this.setState({ todos: todos.map((todo) => ({ ...todo, done })) });
+  };
+  clearAllDone = () => {
+    const { todos } = this.state;
+    this.setState({ todos: todos.filter((todo) => !todo.done) });
+  };
+  render() {
+    const { todos } = this.state;
+    return (
+      <div className="todo-container">
+        <div className="todo-wrap">
+          <Header addTodo={this.addTodo} />
+          <List
+            todos={todos}
+            updateTodo={this.updateTodo}
+            deleteTodo={this.deleteTodo}
+          />
+          <Footer
+            todos={todos}
+            checkAllTodo={this.checkAllTodo}
+            clearAllDone={this.clearAllDone}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
