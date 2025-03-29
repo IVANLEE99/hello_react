c87b3cc85296c9a7ed7f45333f8ad5cb09b0337e
youngs <lixiang@popicorns.com>
2025-03-29 09:21:12 +0800

    feat:官方声明‌：React 19 正式移除了对 prop-types 的支持，推荐使用 TypeScript 进行类型校验‌24。

--- react-staging/src/components/Header/index.jsx
+++ react-staging/src/components/Header/index.jsx
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { nanoid } from "nanoid";
 import "./index.css";
+console.log(PropTypes);
 export default class Header extends Component {
+  static propTypes = {
+    addTodo: PropTypes.func.isRequired,
+  };
   handleKeyUp = (event) => {
     const { keyCode, target } = event;
     if (keyCode === 13) {
