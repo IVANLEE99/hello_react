c00539c79b993ef2764488e727b6c136e61c80e2
youngs <lixiang@popicorns.com>
2025-03-30 08:38:27 +0800

    feat:脚手架配置代理_方法1

--- react-staging/src/App.js
+++ react-staging/src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 class App extends Component {
+  handleClick = () => {
+    axios
+      .get("http://localhost:3000/students")
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   render() {
-    return <div>App</div>;
+    return (
+      <div>
+        <button onClick={this.handleClick}>点击获取学生数据</button>
+      </div>
+    );
   }
 }
 
