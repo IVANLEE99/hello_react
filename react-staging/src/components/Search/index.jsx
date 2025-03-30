bb1767f25aea4ef478995c809fdb438e715ef11b
youngs <lixiang@popicorns.com>
2025-03-30 09:58:35 +0800

    feat:L搜索案例_展示数据

--- react-staging/src/components/Search/index.jsx
+++ react-staging/src/components/Search/index.jsx
@@ -1,13 +1,32 @@
 import React, { Component } from "react";
+import "./index.css";
+import axios from "axios";
 
 export default class Search extends Component {
+  handleSearch = () => {
+    let {
+      keywordElement: { value: keyword },
+    } = this;
+    axios
+      .get(`/api1/search/users?q=${keyword}`)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   render() {
     return (
       <section className="jumbotron">
         <h3 className="jumbotron-heading">Search Github Users</h3>
         <div>
-          <input type="text" placeholder="enter the name you search" />
-          &nbsp;<button>Search</button>
+          <input
+            ref={(c) => (this.keywordElement = c)}
+            type="text"
+            placeholder="enter the name you search"
+          />
+          &nbsp;<button onClick={this.handleSearch}>Search</button>
         </div>
       </section>
     );
