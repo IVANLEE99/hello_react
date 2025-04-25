548890e1ca509e0ee16a16b51e06af06739c63d6
youngs <lixiang@popicorns.com>
2025-04-25 09:36:55 +0800

    feat:redux-count

--- redux_test/src/components/Count/index.jsx
+++ redux_test/src/components/Count/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import store from "../../redux/store";
 
 export default class Count extends Component {
   state = {
@@ -6,29 +7,35 @@ export default class Count extends Component {
   };
   increment = () => {
     const { value } = this.selectNumber;
-    this.setState((state) => ({ count: state.count + value * 1 }));
+    // this.setState((state) => ({ count: state.count + value * 1 }));
+    store.dispatch({ type: "increment", data: value * 1 });
   };
   decrement = () => {
     const { value } = this.selectNumber;
-    this.setState((state) => ({ count: state.count - value * 1 }));
+    // this.setState((state) => ({ count: state.count - value * 1 }));
+    store.dispatch({ type: "decrement", data: value * 1 });
   };
   incrementOdd = () => {
     const { value } = this.selectNumber;
-    if (this.state.count % 2 !== 0) {
-      this.setState((state) => ({ count: state.count + value * 1 }));
+    const count = store.getState();
+    if (count % 2 !== 0) {
+      // this.setState((state) => ({ count: state.count + value * 1 }));
+      store.dispatch({ type: "incrementOdd", data: value * 1 });
     }
   };
   incrementAsync = () => {
     const { value } = this.selectNumber;
     setTimeout(() => {
-      this.setState((state) => ({ count: state.count + value * 1 }));
+      // this.setState((state) => ({ count: state.count + value * 1 }));
+      store.dispatch({ type: "incrementAsync", data: value * 1 });
     }, 500);
   };
   render() {
+    let count = store.getState();
     return (
       <div>
         <h1>Count组件</h1>
-        <h2>当前求和为：{this.state.count}</h2>
+        <h2>当前求和为：{count}</h2>
         <select ref={(c) => (this.selectNumber = c)}>
           <option value="1">1</option>
           <option value="2">2</option>
--- redux_test/src/index.js
+++ redux_test/src/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import store from "./redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// 检测redux中状态的变化，只要变化，就调用render
+store.subscribe(() => {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
