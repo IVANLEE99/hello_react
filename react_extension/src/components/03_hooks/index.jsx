a5e4ea207d2d7e55827c07c90b8392b1390f74c8
youngs <lixiang@popicorns.com>
2025-04-28 18:11:46 +0800

    feat:useEffect

--- react_extension/src/components/03_hooks/index.jsx
+++ react_extension/src/components/03_hooks/index.jsx
@@ -1,32 +1,68 @@
 // import React, { Component } from "react";
-
-// export default class Demo extends Component {
-//   state = { count: 0 };
+// // 正确写法（React 18+）
+// import { RootContext } from "../../index";
+// import { root } from "../../util";
+// class Demo extends Component {
+//   // static contextType = RootContext;
+//   constructor(props) {
+//     super(props);
+//     this.state = { count: 0 };
+//     // console.log(props, this.context, RootContext);
+//     this.rootInstance = root;
+//   }
 //   increment = () => {
 //     const { count } = this.state;
 //     this.setState({ count: count + 1 });
 //   };
+//   destroy = () => {
+//     this.rootInstance.unmount();
+//   };
+//   componentDidMount() {
+//     this.timer = setInterval(() => {
+//       this.setState({ count: this.state.count + 1 });
+//     }, 1000);
+//   }
+//   componentWillUnmount() {
+//     console.log("卸载");
+//     clearInterval(this.timer);
+//   }
 //   render() {
 //     return (
 //       <div>
 //         <h1>当前求和为：{this.state.count}</h1>
 //         <button onClick={this.increment}>点击+</button>
+//         <button onClick={this.destroy}>销毁</button>
 //       </div>
 //     );
 //   }
 // }
-import React, { useState } from "react";
 
+import { root } from "../../util";
+import React, { useState, useEffect } from "react";
 const Demo = () => {
   const [count, setCount] = useState(0);
   const increment = () => {
-    setCount(count + 1);
+    setCount((count) => count + 1);
   };
 
+  useEffect(() => {
+    let timer = setInterval(() => {
+      increment();
+      console.log("dfdf");
+    }, 1000);
+    return () => {
+      console.log("卸载");
+      clearInterval(timer);
+    };
+  }, []);
+  function destroy() {
+    root.unmount();
+  }
   return (
     <div>
       <h1>当前求和为：{count}</h1>
       <button onClick={increment}>点击+</button>
+      <button onClick={destroy}>销毁</button>
     </div>
   );
 };
--- react_extension/src/index.js
+++ react_extension/src/index.js
@@ -1,11 +1,13 @@
+import { root } from "./util";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+export const RootContext = React.createContext();
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <RootContext.Provider value={root}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </RootContext.Provider>
 );
